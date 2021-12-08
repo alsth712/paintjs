0d1efe91f97196b979f6df5f768aa86366b33f8d
alsth712 <alsth712@naver.com>
2021-12-09 04:37:21 +0900

    :hammer: Add rectangle event to app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ const ctx = canvas.getContext('2d');
 // 캔버스 요소 안에서 픽셀에 접근하기 위해선, context variable 을 만드는 것을 먼저 해야함
 const colors = document.getElementsByClassName("jsColor");    // 색상변경 관련
 const colorPicker = document.getElementById("jsColorPicker"); // 컬러픽커 관련
-const range = document.getElementById("jsRange");      // 선 굵기 변경 관련
-const mode = document.getElementById("jsMode");        // fill 버튼 관련
-const saveBtn = document.getElementById("jsSave");     // save 버튼 관련
-const removeBtn = document.getElementById("jsRemove"); // remove 버튼 관련
+const range = document.getElementById("jsRange");             // 선 굵기 변경 관련
+const mode = document.getElementById("jsMode");               // paint 버튼 관련
+const fillBtn = document.getElementById("jsFill");             // fill 버튼 관련
+const saveBtn = document.getElementById("jsSave");            // save 버튼 관련
+const removeBtn = document.getElementById("jsRemove");        // remove 버튼 관련
+const rectBtn = document.getElementById("jsRect");            // 사각형 버튼 관련
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -31,29 +33,54 @@ ctx.lineWidth = 2.5;             // 기본 선 굵기 설정
 
 let painting = false;
 let filling = false;
+let rectDrawing = false;
 
+// 구분 버튼
+let currentPaint = true;
+let currentRect = false;
+
+let startPX = 0; 
+let startPY = 0;
+let startX = 0;
+let startY = 0;
+
+// Paint
 function stopPainting(){
-  painting = false;
-}
-
-function startPainting(){
-  painting = true;
-}
-
-function onMouseMove(event) {
-  // 여기서 모든 움직임을 감지하고 , 라인을 만든다.
-  const X = event.offsetX;
-  const Y = event.offsetY;
-  if(!painting){
-    ctx.beginPath();
-    ctx.moveTo(X,Y);
-  } else {
-    ctx.lineTo(X,Y);
-    ctx.stroke();
-    // 마우스를 움직이는 동안 계속 발생하는 것
+  if(currentPaint === true){
+    painting = false;
   }
 }
 
+function startPainting(){
+  if(currentPaint === true){
+    painting = true;
+  }
+}
+
+function onMouseMove(event) {
+  if(currentPaint === true){
+    const X = event.offsetX;
+    const Y = event.offsetY;
+    if(!painting){
+      ctx.beginPath();
+      ctx.moveTo(X,Y);
+    } else {
+      ctx.lineTo(X,Y);
+      ctx.stroke();
+    } 
+  }
+}
+
+function handleModeClick(){
+  if(!currentPaint){
+    currentPaint = true;
+    filling = false;
+    currentRect = false;
+  }
+}
+
+
+// Color
 function handleColorClick(event){
   localStorage.clear();
   const color = event.target.style.backgroundColor; // 클릭한 컬러를 변수로 저장
@@ -71,19 +98,20 @@ function handleColorPicker(event){
   localStorage.setItem('color', color);
 }
 
+
+// Range
 function handleRangeChange(event){
   const size = event.target.value;
   ctx.lineWidth = size;
 }
 
-function handleModeClick(event){
-  if(filling === true){
-    filling = false;
-    mode.innerText = "Fill";
-  } else {
+
+// fill 
+function handleFillClick(event){
+  if(!filling){
     filling = true;
-    mode.innerText = "Paint";
-    
+    currentRect = false;
+    currentPaint = false;
   }
 }
 
@@ -93,6 +121,8 @@ function handleCanvasClick(){
   }
 }
 
+
+// Save
 function handleCM(event){
   event.preventDefault();
 }
@@ -106,6 +136,54 @@ function handleSaveClick(){
 }
 
 
+// Rect
+function startDrawRect(event){
+  if(currentRect === true){
+    rectDrawing = true;
+    startPX = event.pageX;
+    startPY = event.pageY;
+    startX = event.offsetX;
+    startY = event.offsetY;
+    ctx.beginPath();
+  }
+}
+
+function stopDrawRect(event){
+  if(currentRect === true){
+    rectDrawing = false;
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+function onMouseMoveRect(event){
+  if(currentRect === true){
+    const x = event.pageX;
+    const y = event.pageY;
+    const offsetX = event.offsetX;
+    const offsetY = event.offsetY;
+    if(!rectDrawing){
+      
+    } else {
+      const width = x-startPX;
+      const height = y-startPY;
+      ctx.beginPath();
+      ctx.moveTo(offsetX, offsetY);
+      ctx.rect(startX, startY, width, height);
+    }
+  }
+}
+
+function handleRectClick(){
+  if(!currentRect){
+    currentRect = true;
+    filling = false;
+    currentPaint = false;
+  }
+}
+
+
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove);   // 마우스 움직임 감지
   canvas.addEventListener("mousedown", startPainting); // 마우스 클릭 시, 페인팅 시작
@@ -113,6 +191,11 @@ if(canvas){
   canvas.addEventListener("mouseleave", stopPainting); // 마우스가 캔버스를 벗어나면, 페인팅 종료
   canvas.addEventListener("click", handleCanvasClick); // 캔버스 클릭 시, filling이 true라면 캔버스 사이즈 만큼의 사각형 생성
   canvas.addEventListener("contextmenu", handleCM);    // 오른쪽 마우스 클릭으로 이미지저장 방지
+
+  canvas.addEventListener("mousemove", onMouseMoveRect);   
+  canvas.addEventListener("mousedown", startDrawRect); 
+  canvas.addEventListener("mouseup", stopDrawRect);    
+  canvas.addEventListener("mouseleave", stopDrawRect);
 }
 
 Array.from(colors).forEach(color => 
@@ -132,10 +215,18 @@ if(mode){
   mode.addEventListener("click", handleModeClick)
 }
 
+if(fillBtn){
+  fillBtn.addEventListener("click", handleFillClick)
+}
+
 if(saveBtn){
   saveBtn.addEventListener("click", handleSaveClick)
 }
 
 if(removeBtn){
   removeBtn.addEventListener("click", defaultRect)
+}
+
+if(rectBtn){
+  rectBtn.addEventListener("click", handleRectClick)
 }
